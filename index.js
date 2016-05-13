f0155db3cfbb9aee323075b35e0b732949e3e128
hongee <hongee.tc@gmail.com>
2016-05-13 11:21:41 -0700

    script scaffold

--- index.js
+++ index.js
@@ -1,33 +1,79 @@
 var gcloud = require('gcloud');
 var fs = require('fs');
 var _ = require('lodash');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+app.use(express.static('static'));
 
 var gvision = gcloud.vision({
     projectId: 'e96--1282',
     keyFilename: './key.json'
 });
 
-var img = fs.readFile("./img.jpg", function(err, data) {
-    var imgb64 = data.toString('base64');
-    var annotateImageReq = {
-        "image": {
-            "content": imgb64
-        },
-        "features": [
-            {
-                "type": "LABEL_DETECTION"
-            }
-        ]
-    };
+function uploadImageToGcloud() {
+  fs.readFile("test003.jpg", function(err, data) {
+      var imgb64 = data.toString('base64');
+      var annotateImageReq = {
+          "image": {
+              "content": imgb64
+          },
+          "features": [
+              {
+                  "type": "LABEL_DETECTION"
+              }
+          ]
+      };
 
-    console.log("Polling Vision API...");
+      console.log("Polling Vision API...");
 
-    gvision.annotate(annotateImageReq, function(err, annotations, apiResponse) {
-      console.log("Done!\n");
-      _.forEach(annotations[0] ,function(val) {
-        _.forEach(val, function(v) {
-          console.log(v.description);
-        })
+      gvision.annotate(annotateImageReq, function(err, annotations, apiResponse) {
+        console.log("Done!\n");
+        _.forEach(annotations[0] ,function(val) {
+          _.forEach(val, function(v) {
+            console.log(v.description);
+          })
+        });
       });
-    });
+  });
+}
+
+function snap(res) {
+  exec("/home/root/bin/ffmpeg/ffmpeg -s 640x480 -f video4linux2 -i /dev/video0 -vframes 3 test%3d.jpg" , function(error, stdout, stderr) {
+    //get file information for taken image
+    var stats = fs.statSync("test003.jpg");
+    console.log(stats["size"]);
+    if(stats["size"] < 10000) {
+      console.log("bad img");
+      snap(res);
+    } else {
+      console.log("good img");
+      fs.readFile("test003.jpg", function(err, data) {
+          var imgb64 = data.toString('base64');
+          res.json({
+            img: imgb64,
+          });
+      });
+    }
+
+  });
+}
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/app/index.html');
+});
+
+app.get('/api/snap', function(req,res) {
+  snap(res);
+});
+
+app.get('/api/identify', function(req,res) {
+  uploadImageToGcloud();
+});
+
+http.listen(3000, function() {
+  console.log("Server running on port 3000");
 });
